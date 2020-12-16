7ba23e948ae473f8a83cd313ba1d8666245395c8
Akita Emmanuel <e.akita91@gmail.com>
2020-12-16 15:52:02 +0100

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import { withAuthentication } from './component/session';
+import Login from './component/Auth/login';
+import ErrorPageContainer from './container/404';
+import Layout from './component/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Layout>
+          <Switch>
+          <Route path='/' exact component={Login} />
+          <Route component={ErrorPageContainer} />
+          </Switch>
+      </Layout>
     </div>
   );
 }
 
-export default App;
+export default withAuthentication(App);
