0f9076dbb99b16506108bf9d1069ca21442720aa
Akita Emmanuel <e.akita91@gmail.com>
2020-12-16 15:51:23 +0100

    Add FirebaseContext Provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import Firebase, { FirebaseContext } from './component/Firebase';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <FirebaseContext.Provider value={new Firebase()}>
+     <BrowserRouter>
+         <React.StrictMode>
+           <App />
+        </React.StrictMode>
+      </BrowserRouter>
+    </FirebaseContext.Provider>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
